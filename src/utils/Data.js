98139bdb13adfcf4419c5d3a1b77bf647b4b0e98
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-05-16 00:46:44 +0530

    data for social media icons added

---
+++ src/utils/Data.js
@@ -0,0 +1,19 @@
+import {AiFillGithub} from 'react-icons/ai';
+import {AiFillGoogleCircle} from 'react-icons/ai';
+import {BsFacebook} from 'react-icons/bs';
+
+
+export const SocialLinks=[
+    {
+        id:1,
+        icon:<AiFillGithub/>
+    },
+    {
+        id:2,
+        icon:<AiFillGoogleCircle/>
+    },
+    {
+        id:3,
+        icon:<BsFacebook/>
+    },
+];
